ad0a1bf64ad991ccf3296bb8dc6968b86f5f7cc9
Ali Keramatipour <alikeramatipour@gmail.com>
2020-01-24 13:58:30 +0330

    tested and debugged fifo_mem

--- fifo_mem.v
+++ fifo_mem.v
@@ -6,7 +6,7 @@ module fifomem #(parameter WORDSIZE = 8,
                input [WORDSIZE-1:0] wdata,
                input full, wclk);
 
-               reg [WORDSIZE-1:0] mem [0:ADDRSIZE-1];
+               reg [WORDSIZE-1:0] mem [0:(1<<ADDRSIZE)-1];
 
                assign rdata = mem[raddr];
                always @(posedge wclk) begin
--- fifomem_TB.v
+++ fifomem_TB.v
@@ -9,22 +9,34 @@ module fifomem_TB();
       fifomem #(WORDSIZE,ADDRSIZE) UUT(waddr, raddr, rdata , wdata, full, clk);
      always #100 clk = ~clk;
      initial begin
-       #50  wdata = 3'd100;
-            waddr = 3'b000;
-       #150 wdata = 3'd101;
-            waddr = 3'b001;
-       #250 wdata = 3'd101;
-            waddr = 3'b010;
-       #350 wdata = 3'd102;
-            waddr = 3'b011;
-       #450 wdata = 3'd103;
-            waddr = 3'b100;
-       #550 wdata = 3'd104;
-            waddr = 3'b101;
-       #650 wdata = 3'd105;
-            waddr = 3'b110;
-       #750 wdata = 3'd106;
-            waddr = 3'b111;
+          #50  wdata =  'd100;
+               waddr = 3'b000;
+               
+          #150 wdata =  'd101;
+               waddr = 3'b001;
+               
+          #250 wdata =  'd102;
+               waddr = 3'b010;
+               
+          #350 wdata =  'd103;
+               waddr = 3'b011;
+               
+          #450 wdata =  'd104;
+               waddr = 3'b100;
+               
+          #550 wdata =  'd105;
+               waddr = 3'b101;
+               
+          #650 wdata =  'd106;
+               waddr = 3'b110;
+               
+          #750 wdata =  'd107;
+               waddr = 3'b111;
+               
+          #850 raddr = 3'b010;
+          #950 raddr = 3'b011;
+          #1050 raddr = 3'b100;
+       $stop;
      end
      
 endmodule
