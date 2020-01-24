d79807f00bc1cec2ec1500c0b23ec2469e8e6f4e
Ali Keramatipour <alikeramatipour@gmail.com>
2020-01-24 13:25:01 +0330

    fifomem_TB added

--- fifomem_TB.v
+++ fifomem_TB.v
@@ -1,5 +1,30 @@
 `timescale 1 ns / 1 ps
-module fifomem_TB #(parameter WORDSIZE = 8,
-                parameter ADDRSIZE = 8)
-              ();
+module fifomem_TB();
+      parameter WORDSIZE = 8;
+      parameter ADDRSIZE = 3;
+      reg [ADDRSIZE-1:0] waddr = 0, raddr = 0;
+      reg [WORDSIZE-1:0] wdata = 0;
+      reg full = 0;
+      reg clk = 0;
+      fifomem #(WORDSIZE,ADDRSIZE) UUT(waddr, raddr, rdata , wdata, full, clk);
+     always #100 clk = ~clk;
+     initial begin
+       #50  wdata = 3'd100;
+            waddr = 3'b000;
+       #150 wdata = 3'd101;
+            waddr = 3'b001;
+       #250 wdata = 3'd101;
+            waddr = 3'b010;
+       #350 wdata = 3'd102;
+            waddr = 3'b011;
+       #450 wdata = 3'd103;
+            waddr = 3'b100;
+       #550 wdata = 3'd104;
+            waddr = 3'b101;
+       #650 wdata = 3'd105;
+            waddr = 3'b110;
+       #750 wdata = 3'd106;
+            waddr = 3'b111;
+     end
+     
 endmodule
